6991fbdcf9fb72b29ae329134fe4b33f65557820
Matias Nicotra <matiasanicotra@gmail.com>
2023-12-29 13:18:36 -0300

    se encarga de añadir un event listener al elemento con el ID o referencia inputCard para el evento

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ let cardNumber = [];
 let dateNumber = [];
 let cvvNumber = [];
 
+
+inputCard.addEventListener("keydown", (e) => {
+    if (e.key = 'Tab') {
+        return;
+    }
+    
+    e.preventDefault();
+    handleInput(maskNumber, e.key, cardNumber);
+    inputCard.value = cardNumber.join("");
+});
