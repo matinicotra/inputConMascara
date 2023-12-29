f77270a12181dffad8f865797ce2d3f0eb342de3
Matias Nicotra <matiasanicotra@gmail.com>
2023-12-29 20:08:11 -0300

    function handleInput

--- main.js
+++ main.js
@@ -16,8 +16,26 @@ inputCard.addEventListener("keydown", (e) => {
     if (e.key = 'Tab') {
         return;
     }
-    
+
     e.preventDefault();
     handleInput(maskNumber, e.key, cardNumber);
     inputCard.value = cardNumber.join("");
 });
+
+function handleInput(mask, key, arr) {
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    if (key == "Backspace" && arr.length > 0) {
+        arr.pop();
+        return;
+    }
+
+    if (numbers.includes(key) && arr.length + 1 <= mask.length) {
+        if (mask[arr.length] == "-" || mask[arr.length] == "/") {
+            arr.push(mask[arr.length], key);
+        }
+        else {
+            arr.push(key);
+        }
+    }
+}
